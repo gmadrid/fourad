1e1ced0e68862d9acdb47ec4739b18407699d643
George Madrid <gmadrid@gmail.com>
2021-09-14 09:22:51 -0400

    Added --explode switch.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use argh::FromArgs;
 struct Args {
     #[argh(positional)]
     codes: Vec<String>,
+
+    /// if set, all d6 rolls will 'explode' (keep rolling on a '6').
+    #[argh(switch, short='X')]
+    explode: bool,
 }
 
 fn main() -> fourad::Result<()> {
@@ -20,7 +24,7 @@ fn main() -> fourad::Result<()> {
             println!("{}", code);
         }
         // TODO: add an --explodes switch
-        println!("===> {}", fourad::roll(&code, true)?);
+        println!("===> {}", fourad::roll(&code, args.explode)?);
         if print_codes {
             println!()
         }
